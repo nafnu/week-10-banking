f5f837b8854475269bc8852a97c2f76c58216e73
Nathalie Flores <21250@student.dorset-college.ie>
2020-11-24 10:54:34 -0600

    four commit CustomerCollection

--- src/com/jetbrains/CustomerCollection.java
+++ src/com/jetbrains/CustomerCollection.java
@@ -1,4 +1,8 @@
 package com.jetbrains;
 
-public class CustomerCollection {
+public class CustomerCollection extends Customer {
+
+    public CustomerCollection(String f, String l, String iA, int cp) {
+        super(f, l, iA, cp);
+    }
 }
--- src/com/jetbrains/Transaction.java
+++ src/com/jetbrains/Transaction.java
@@ -1,4 +1,5 @@
 package com.jetbrains;
 
 public abstract class Transaction {
+
 }
